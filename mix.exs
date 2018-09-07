3ddaeb32bc84fe22b42541045903863f3df4ca10
Boyd Multerer <boyd@multerer.com>
2018-09-07 02:02:43 -0700

    add docs

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule ScenicClock.MixProject do
     [
       # {:scenic, "~> <%= @scenic_version %>"},
       {:scenic, git: "git@github.com:boydm/scenic.git"},
-      {:timex, "~> 3.3"}
+      {:timex, "~> 3.3"},
 
+      {:ex_doc, ">= 0.0.0", only: [:dev, :docs]},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
